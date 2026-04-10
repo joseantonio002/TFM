7cdd1e0c4ad9bdcdf99c22e04e90fad55868e46b
Jose <joseantunezpul@gmail.com>
2026-04-10 18:36:14 +0100

    connecting docker to airflow

--- services/minimal_airflow/dags/doc.py
+++ services/minimal_airflow/dags/doc.py
@@ -13,7 +13,7 @@ with DAG(
         task_id="run_hello_world",
         image="hello-world",              # imagen a ejecutar
         api_version="auto",
-        auto_remove="force",                # elimina el contenedor al terminar
+        auto_remove="never",                # elimina el contenedor al terminar
         command=None,                    # hello-world ya tiene CMD
         docker_url="unix://var/run/docker.sock",  # conexión al daemon Docker
         network_mode="bridge",
