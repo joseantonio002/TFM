22466de36f413f1497184edf09937af507daa4e4
Jose <joseantunezpul@gmail.com>
2026-04-20 23:27:29 +0100

    ner with spacy

---
+++ services/pipeline_NLP/NER/test.py
@@ -0,0 +1,12 @@
+import spacy
+
+nlp = spacy.load(
+    "es_core_news_lg",
+    disable=["morphologizer", "parser", "attribute_ruler", "lemmatizer", "senter"]
+)
+
+
+print(nlp.get_pipe("ner").labels)
+
+doc = nlp("Madrid es la capital de España.")
+print([(ent.text, ent.label_) for ent in doc.ents])
